d3ee7c34169271c5f569b1135222651816124f03
student <student@K04-03.lan.itstep.kiev.ua>
2018-05-29 19:30:36 +0300

    Ya Ya Commit

---
+++ TestName/src/org/itstep/Name.java
@@ -0,0 +1,14 @@
+package org.itstep;
+
+public class Name {
+
+	public static void main(String[] args) {
+	
+		int a = 5;
+		int b = 2;
+		int c = a*b;
+		System.out.println(c);
+
+	} // Name
+
+} // main
